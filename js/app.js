b2714ecf0fade362f11a1523f7ed7d62e0c17458
TimurG1 <timur.ganziy@gmail.com>
2026-01-03 19:13:16 +0100

    1.0

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const friends = ["Mango", "Poly", "Kiwi", "Ajax"];
+let string = friends.join(" ")
+
+const cards = [
+    "Карточка-1",
+    "Карточка-2",
+    "Карточка-3",
+    "Карточка-4",
+    "Карточка-5",
+];
+const cardToRemuve = cards.indexOf("Карточка-3");
+const removedCard =  cards.splice(cardToRemuve, 1);
+
+const cardToInsert = "Карточка-6";
+const insertCard = cards.push(cardToInsert);
+
+const cardToUpdate = cards.indexOf("Карточка-4");
+const cardsUpdated = cards.splice(cardToUpdate, 0,);
